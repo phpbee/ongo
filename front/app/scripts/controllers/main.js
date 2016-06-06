b09c3e3ed21b8a606fd2a923bc8abe667c145173
Alexei Kochetov <alex@bonusbay.com>
2016-06-06 07:27:18 +0300

    angularjs frontend generated by yeoman

---
+++ front/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the frontApp
+ */
+angular.module('frontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
