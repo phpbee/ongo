2a8cff5012dd9f18acdd78f2c3b979fcdae12158
Alexei Kochetov <alex@bonusbay.com>
2016-06-30 00:04:02 +0300

    img resize scripts

---
+++ scripts/convert_800.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+l=$1
+b=`basename "${l}"`
+d=`dirname "${l}"`
+td=$d/thumb
+if [ ! -d "$td" ]; then
+	mkdir -v "$td"
+fi
+convert "$l" -resize '480000@' -verbose "$l"
+convert "$l" -resize '117600@' -verbose "$td/$b"
