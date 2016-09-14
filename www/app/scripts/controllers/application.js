9c80970ad7446d56d882e418fd32c3921140455d
Alexei Kochetov <alex@bonusbay.com>
2016-09-14 15:55:29 +0300

    save session to the storage

--- www/app/scripts/controllers/application.js
+++ www/app/scripts/controllers/application.js
@@ -8,7 +8,7 @@
  * Controller of the wwwApp
  */
 angular.module('wwwApp')
-    .controller('ApplicationCtrl', function ($rootScope, $scope, $stateParams, $state, $translate, AuthService) {
+    .controller('ApplicationCtrl', function ($rootScope, $scope, $stateParams, $state, $translate, AuthService, Session, storage) {
 
         $scope.changeLanguage = function (key) {
             $scope.activeLanguage = key;
@@ -23,6 +23,7 @@ angular.module('wwwApp')
         $rootScope.currentUser = null;
         $rootScope.isAuthorized = AuthService.isAuthorized;
         $rootScope.$state = $state;
+
         $rootScope.requireAuth = function($state, role) {
             var roles = $state.$current.data && $state.$current.data.authorizedRoles ? $state.$current.data.authorizedRoles : [];
             if (!angular.isArray(roles)) {
@@ -30,5 +31,11 @@ angular.module('wwwApp')
             }
             var b = roles.indexOf(role) !== -1;
             return b;
+        };
+
+
+        var session = storage.get('session');
+        if (session) {
+            Session.create(session.token, session.user_id, session.role);
         }
     });
