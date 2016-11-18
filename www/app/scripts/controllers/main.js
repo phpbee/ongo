ab9fea68c078de2e756303300f7e9621165fffd8
alex <alex@kochetov.com>
2016-11-18 12:19:49 +0300

    search form

--- www/app/scripts/controllers/main.js
+++ www/app/scripts/controllers/main.js
@@ -10,20 +10,25 @@
 angular.module('wwwApp')
     .controller('MainCtrl', function ($scope, Photograph, Place, City, Country, Gallery) {
 
-        $scope.topCountry = Country.top();
-        $scope.topCity = City.top();
-        $scope.topPlace = Place.top();
-        $scope.topGalleries = Gallery.top({limit: 3});
+        //$scope.topCountry = Country.top();
+        //$scope.topCity = City.top();
+        //$scope.topPlace = Place.top();
 
-        $scope.photographers = Photograph.query({limit: 5});
-        $scope.places = Place.query({limit: 5});
-        $scope.galleries = Gallery.query({limit: 8},
-            function (data) {
-                angular.forEach(data, function (row) {
-                    row.date = new Date(row.created);
-                    row.icons = Gallery.icons({id: row.id, limit: 10});
-                });
-            }
-        );
+        //$scope.photographers = Photograph.query({limit: 5});
+        //$scope.places = Place.query({limit: 5});
+
+
+        Gallery.top({limit: 3}, function (galleries) {
+            $scope.topGalleries = galleries;
+            $scope.galleries = Gallery.query({limit: 8},
+                function (data) {
+                    angular.forEach(data, function (row) {
+                        row.date = new Date(row.created);
+                        row.icons = Gallery.icons({id: row.id, limit: 10});
+                    });
+                }
+            );
+        });
+
 
     });
