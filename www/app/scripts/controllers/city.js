9bfd85fcf25d0239dcf2cca708f433b78ffccc6a
vlad <vssmirnov@gmail.com>
2017-10-19 10:11:22 -0400

    city gallery preview

--- www/app/scripts/controllers/city.js
+++ www/app/scripts/controllers/city.js
@@ -8,9 +8,14 @@
  * Controller of the wwwApp
  */
 angular.module('wwwApp')
-  .controller('CityCtrl', function ($scope, $stateParams, City) {
+  .controller('CityCtrl', function ($scope, $stateParams, City, Gallery) {
 
       $scope.city = City.get({id: $stateParams.id});
-      $scope.galleries = City.galleries({id: $stateParams.id});
+      $scope.galleries = City.galleries({id: $stateParams.id}, function (data) {
+        angular.forEach(data, function (row) {
+          row.date = new Date(row.created);
+          row.icons = Gallery.icons({id: row.id, limit: 6});
+        });
+      });
 
   });
